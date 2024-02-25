6e5feb5c499303936dea3c462b3fc81bec4fecb6
Jerome Kelleher <jk@well.ox.ac.uk>
2024-02-25 23:36:34 +0000

    Basic docs

--- bio2zarr/cli.py
+++ bio2zarr/cli.py
@@ -35,6 +35,9 @@ def setup_logging(verbosity):
 @worker_processes
 @click.option("-c", "--column-chunk-size", type=int, default=64)
 def explode(vcfs, out_path, verbose, worker_processes, column_chunk_size):
+    """
+    Convert VCF(s) to columnar intermediate format
+    """
     setup_logging(verbose)
     vcf.explode(
         vcfs,
@@ -49,6 +52,9 @@ def explode(vcfs, out_path, verbose, worker_processes, column_chunk_size):
 @click.argument("if_path", type=click.Path())
 @verbose
 def inspect(if_path, verbose):
+    """
+    Inspect an intermediate format file
+    """
     setup_logging(verbose)
     data = vcf.inspect(if_path)
     click.echo(tabulate.tabulate(data, headers="keys"))
@@ -57,6 +63,9 @@ def inspect(if_path, verbose):
 @click.command
 @click.argument("if_path", type=click.Path())
 def mkschema(if_path):
+    """
+    Generate a schema for zarr encoding
+    """
     stream = click.get_text_stream("stdout")
     vcf.mkschema(if_path, stream)
 
@@ -68,6 +77,9 @@ def mkschema(if_path):
 @click.option("-s", "--schema", default=None)
 @worker_processes
 def encode(if_path, zarr_path, verbose, schema, worker_processes):
+    """
+    Encode intermediate format (see explode) to vcfzarr
+    """
     setup_logging(verbose)
     vcf.encode(
         if_path,
@@ -84,6 +96,9 @@ def encode(if_path, zarr_path, verbose, schema, worker_processes):
 @verbose
 @worker_processes
 def convert_vcf(vcfs, out_path, verbose, worker_processes):
+    """
+    Convert input VCF(s) directly to vcfzarr (not recommended for large files)
+    """
     setup_logging(verbose)
     vcf.convert(vcfs, out_path, show_progress=True, worker_processes=worker_processes)
 
@@ -101,6 +116,7 @@ def vcf2zarr():
     pass
 
 
+# TODO figure out how to get click to list these in the given order.
 vcf2zarr.add_command(explode)
 vcf2zarr.add_command(inspect)
 vcf2zarr.add_command(mkschema)
@@ -116,6 +132,9 @@ vcf2zarr.add_command(validate)
 @click.option("--chunk-width", type=int, default=None)
 @click.option("--chunk-length", type=int, default=None)
 def convert_plink(in_path, out_path, worker_processes, chunk_width, chunk_length):
+    """
+    In development; DO NOT USE!
+    """
     plink.convert(
         in_path,
         out_path,
