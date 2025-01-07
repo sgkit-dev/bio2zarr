d8f72c95f2437825a69edc448615f6368f95c40c
Tom White <tom.e.white@gmail.com>
2025-01-07 12:18:41 +0000

    Add `first_dim_iter` utility to workaround slow array iteration in zarr-python v3

--- bio2zarr/zarr_utils.py
+++ bio2zarr/zarr_utils.py
@@ -11,3 +11,9 @@ if zarr_v3():
     ZARR_FORMAT_KWARGS = dict(zarr_format=2)
 else:
     ZARR_FORMAT_KWARGS = dict()
+
+
+# See discussion in https://github.com/zarr-developers/zarr-python/issues/2529
+def first_dim_iter(z):
+    for chunk in range(z.cdata_shape[0]):
+        yield from z.blocks[chunk]
