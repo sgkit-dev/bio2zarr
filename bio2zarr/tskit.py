7b70e97ed5ea16388e5f76aa1bc802dd36db31b2
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2025-05-09 08:13:49 +0100

    Add tskit CLI

--- bio2zarr/tskit.py
+++ bio2zarr/tskit.py
@@ -13,7 +13,7 @@ class TskitFormat(vcz.Source):
     def __init__(
         self,
         ts_path,
-        individuals_nodes,
+        individuals_nodes=None,
         sample_ids=None,
         contig_id=None,
         isolated_as_missing=False,
@@ -24,6 +24,13 @@ class TskitFormat(vcz.Source):
         self.isolated_as_missing = isolated_as_missing
 
         self.positions = self.ts.sites_position
+        if individuals_nodes is None:
+            if self.ts.num_individuals == 0:
+                raise ValueError(
+                    "No individuals found in the tree sequence, use individuals_nodes "
+                    "argument to specify the individuals nodes"
+                )
+            individuals_nodes = self.ts.individuals_nodes
 
         self._num_samples = individuals_nodes.shape[0]
         if self._num_samples < 1:
@@ -213,8 +220,8 @@ class TskitFormat(vcz.Source):
 def convert(
     ts_path,
     zarr_path,
-    individuals_nodes,
     *,
+    individuals_nodes=None,
     sample_ids=None,
     contig_id=None,
     isolated_as_missing=False,
@@ -225,7 +232,7 @@ def convert(
 ):
     tskit_format = TskitFormat(
         ts_path,
-        individuals_nodes,
+        individuals_nodes=individuals_nodes,
         sample_ids=sample_ids,
         contig_id=contig_id,
         isolated_as_missing=isolated_as_missing,
