a8f47468293c731b447b3de8f60ea8bd97fda5ea
Ben Jeffery <ben.jeffery@bdi.ox.ac.uk>
2026-01-05 11:16:56 +0000

    Fix CI

--- docs/build.sh
+++ docs/build.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/usr/bin/env bash
 
 # Jupyter-build doesn't have an option to automatically show the 
 # saved reports, which makes it difficult to debug the reasons for 
@@ -6,7 +6,7 @@
 
 REPORTDIR=_build/html/reports
 
-jupyter-book build -Wn --keep-going .
+jupyter-book build -W -n --keep-going .
 RETVAL=$?
 if [ $RETVAL -ne 0 ]; then
     if [ -e $REPORTDIR ]; then
