8f60a277882739edca4bf37d62340a048868c1a1
Jerome Kelleher <jk@well.ox.ac.uk>
2026-04-01 13:56:27 +0100

    Fix docs build to use uv run
    Update build.sh to use `uv run --project=.. --group docs` like tsdate,
    removing the need for PYTHONPATH hacks. Simplify Makefile accordingly.

--- docs/build.sh
+++ docs/build.sh
@@ -1,12 +1,12 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Jupyter-build doesn't have an option to automatically show the 
-# saved reports, which makes it difficult to debug the reasons for 
+# Jupyter-build doesn't have an option to automatically show the
+# saved reports, which makes it difficult to debug the reasons for
 # build failures in CI. This is a simple wrapper to handle that.
 
 REPORTDIR=_build/html/reports
 
-jupyter-book build -W -n --keep-going .
+uv run --project=.. --group docs jupyter-book build . -vnW --keep-going
 RETVAL=$?
 if [ $RETVAL -ne 0 ]; then
     if [ -e $REPORTDIR ]; then
